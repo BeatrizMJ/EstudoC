fe5629a59057066863d08aeac5a0c716d4ebf947
Beatriz Moresco Joaquim <126287049+BeatrizMJ@users.noreply.github.com>
2025-02-04 19:17:42 -0300

    Update inimigos.h

--- JogoRPG/inimigos.h
+++ JogoRPG/inimigos.h
@@ -6,10 +6,10 @@ typedef struct Inimigo {
     struct Inimigo *prox;
 } Inimigo;
 
-void inicializar_inimigo(int x, int y);
-Inimigo *adicionar_inimigo(int x, int y, char **tabuleiro, int N);
-void mover_inimigos(Inimigo *inimigos, char **tabuleiro);
-void remover_inimigo(Inimigo *inimigos, Inimigo *alvo); //
-void liberar_memoria_inimigos(Inimigo *inimigos);
+void adicionar_inimigo_lista(Inimigo *novo_inimigo);
+Inimigo *criar_inimigo(int x, int y, char **tabuleiro, int N);
+//void mover_inimigos(Inimigo *inimigos, char **tabuleiro);
+void remover_inimiges(Inimigo *alvo); //
+void liberar_memoria_inimigos();
 
 #endif
