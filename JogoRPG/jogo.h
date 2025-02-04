9bdfff8c4cb3f623a65dc4f6bfa3ac4de7cb0da6
Beatriz Moresco Joaquim <126287049+BeatrizMJ@users.noreply.github.com>
2025-02-04 19:19:32 -0300

    Update jogo.h

--- JogoRPG/jogo.h
+++ JogoRPG/jogo.h
@@ -1,12 +1,7 @@
 #ifndef JOGO_H
 #define JOGO_H
 
-#include "tabuleiro.h"
-#include "personagem.h"
-#include "inimigos.h"
-#include "itens.h"
-
 void jogo();
-void liberar_memoria(char **tabuleiro, int N, Inimigo *inimigos, Item *itens, Personagem *p);
+void liberar_memoria(int N);
 
 #endif
